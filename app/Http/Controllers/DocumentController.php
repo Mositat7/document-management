08c2b42ea0ce0db5283635d7e70ccb069ef263ff
mositat7 <mositat7@gmail.com>
2025-12-18 13:58:01 +0330

    feat(document): add service for generating persian document code

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -8,7 +8,17 @@ class DocumentController extends Controller
 {
     public function index() {}
     public function create() {}
-    public function store(Request $request) {}
+        public function store(Request $request, DocumentCodeGenerator $codeGenerator)
+    {
+        $document = Document::create([
+            'title'     => $request->title,
+            'type'      => $request->type,
+            'receiver'  => $request->receiver,
+            'sender'    => 'کاربر جاری', // فعلاً ثابت
+            'status'    => 'draft',
+            'code'      => $codeGenerator->generate(),
+            'content'   => $request->content,
+        ]);
     public function show(Document $document) {}
     public function download(Document $document, DocumentAttachment $attachment) {}
     public function destroy(Document $document) {}
