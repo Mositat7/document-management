f54d78f4d19e3cf672f4cb7169c964a79cb81ec5
mositat7 <mositat7@gmail.com>
2025-12-18 13:50:20 +0330

    feat: define document and attachment relationships

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -4,7 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Attachment extends Model
+class DocumentAttachment extends Model
 {
-    //
+    protected $fillable = [
+        'document_id',
+        'original_name',
+        'file_path',
+        'size',
+    ];
+
+    public function document()
+    {
+        return $this->belongsTo(Document::class);
+    }
 }
+
--- app/Models/Document.php
+++ app/Models/Document.php
@@ -4,7 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Document extends Model
+
+  class Document extends Model
 {
-    //
+    protected $fillable = [
+        'code',
+        'title',
+        'type',
+        'sender',
+        'receiver',
+        'body',
+        'status',
+        'registered_at',
+    ];
+
+    public function attachments()
+    {
+        return $this->hasMany(DocumentAttachment::class);
+    }
 }
