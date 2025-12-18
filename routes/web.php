167fddab6a9e350cca76695d71710af1ca7842ae
mositat7 <mositat7@gmail.com>
2025-12-18 13:39:00 +0330

    feat: add document routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/documents', [DocumentController::class, 'index']);
+Route::get('/documents/create', [DocumentController::class, 'create']);
+Route::post('/documents', [DocumentController::class, 'store']);
+Route::get('/documents/{document}', [DocumentController::class, 'show']);
+Route::get('/documents/{document}/download/{attachment}', [DocumentController::class, 'download']);
+Route::delete('/documents/{document}', [DocumentController::class, 'destroy']);
