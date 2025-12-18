c3d198415359f639c93f2e866206769226ad67eb
mositat7 <mositat7@gmail.com>
2025-12-18 14:46:59 +0330

    feat(document): add edit route for draft documents

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DocumentController;
-Route::get('/documents', [DocumentController::class, 'index']);
-Route::get('/documents/create', [DocumentController::class, 'create']);
-Route::post('/documents', [DocumentController::class, 'store']);
-Route::get('/documents/{document}', [DocumentController::class, 'show']);
-Route::get('/documents/{document}/download/{attachment}', [DocumentController::class, 'download']);
-Route::delete('/documents/{document}', [DocumentController::class, 'destroy']);
+
+Route::get('/documents', [DocumentController::class, 'index'])->name('documents.index');
+Route::get('/documents/create', [DocumentController::class, 'create'])->name('documents.create');
+Route::post('/documents', [DocumentController::class, 'store'])->name('documents.store');
+Route::get('/documents/{document}/edit', [DocumentController::class, 'edit'])->name('documents.edit'); // ⬅️ جدید
+Route::get('/documents/{document}', [DocumentController::class, 'show'])->name('documents.show');
+Route::get('/documents/{document}/download/{attachment}', [DocumentController::class, 'download'])->name('documents.download');
+Route::delete('/documents/{document}', [DocumentController::class, 'destroy'])->name('documents.destroy');
